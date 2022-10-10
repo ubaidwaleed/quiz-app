99d73836fbc05ddbedb2e3920fe84b430299d22a
ubaidwaleed <ubaidwaleed1@gmail.com>
2022-10-10 16:39:09 +0500

    a

--- src/services/quiz_service.ts
+++ src/services/quiz_service.ts
@@ -1,5 +1,15 @@
-export const getQuizDetails=async (totalQuestions:number,level:string)=>{
-const res= await fetch(`https://opentdb.com/api.php?amount=${totalQuestions}&difficulty=${level}&type=multiple`)
+import {Quiz,QuestionType} from './../types/quiz_types'
+
+export const getQuizDetails=async (totalQuestions:number,level:string):Promise<Quiz[]>=>{
+const res= await fetch(`https://opentdb.com/api.php?amount=${totalQuestions}&difficulty=${level}&type=multiple`);
 let {results}=await res.json();
-console.log(results);
+const quiz=results.map((questionObj:Quiz,ind:number)=>{
+    return{
+        question:questionObj.question,
+        answer:questionObj.correct_answer,
+        option:questionObj.incorrect_answers.concat(questionObj.correct_answer)
+    }
+});
+//const results=data.results.map
+return results;
 }
